56b2720468248d9e69d182f67c4a61b70d90724f
John <john.ce@gmail.com>
2020-05-02 22:12:06 -0500

    Updates

---
+++ precache-manifest.989b2240fb413c4553f1e6375201ad06.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c48a9fd142658abf47300350dc8e152f",
+    "url": "/reactjs-monster/index.html"
+  },
+  {
+    "revision": "3f723abbe9354f91fc48",
+    "url": "/reactjs-monster/static/css/main.37ccd4d2.chunk.css"
+  },
+  {
+    "revision": "a2e4be9481a54b890f97",
+    "url": "/reactjs-monster/static/js/2.7aaef059.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/reactjs-monster/static/js/2.7aaef059.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3f723abbe9354f91fc48",
+    "url": "/reactjs-monster/static/js/main.eab90eba.chunk.js"
+  },
+  {
+    "revision": "0c904a015096428a885f",
+    "url": "/reactjs-monster/static/js/runtime-main.57b70c49.js"
+  }
+]);
